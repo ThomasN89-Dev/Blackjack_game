89dbd0c52066f0e51db0636b6f8803891c740678
LAPTOP-C1S6L0E6\ACER <t.neroni@reply.it>
2023-01-09 16:34:34 +0100

    added visual cards

--- index.js
+++ index.js
@@ -8,24 +8,85 @@ let gameWon = 0;
 const messageEl = document.getElementById("message-el");
 const sumEl = document.querySelector("#sum-el");
 const cardEl = document.querySelector("#cards-el");
-let gamesPlayed = document.querySelector("#games-played");
-let gamesWon = document.querySelector("#games-won");
+const gamesPlayed = document.querySelector("#games-played");
+const gamesWon = document.querySelector("#games-won");
+const cardsGallery = document.querySelector(".cards-holder");
 gamesPlayed.textContent = "Partite giocate: ";
 gamesWon.textContent = "Partite vinte: ";
+let cardNumber;
 
 function getRandomCard() {
   let randNumber = Math.floor(Math.random() * 13) + 1;
+  switch (randNumber) {
+    case 1:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/01_of_spades_A.svg/1200px-01_of_spades_A.svg.png";
+      break;
+    case 2:
+      cardSeed = 2;
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Playing_card_spade_2.svg/614px-Playing_card_spade_2.svg.png";
+      break;
+    case 3:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_spade_3.svg/1638px-Playing_card_spade_3.svg.png";
+      break;
+    case 4:
+      cardNumber =
+        "https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/74939/four-of-spades-clipart-xl.png";
+      break;
+    case 5:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/5_of_spades.svg/706px-5_of_spades.svg.png";
+      break;
+    case 6:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/6_of_spades.svg/1200px-6_of_spades.svg.png";
+      break;
+    case 7:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/7_of_spades.svg/1200px-7_of_spades.svg.png";
+      break;
+    case 8:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/8_of_spades.svg/1200px-8_of_spades.svg.png";
+      break;
+    case 9:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/9_of_spades.svg/1200px-9_of_spades.svg.png";
+      break;
+    case 10:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/10_of_spades.svg/1200px-10_of_spades.svg.png";
+      break;
+    case 11:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/English_pattern_jack_of_spades.svg/360px-English_pattern_jack_of_spades.svg.png";
+      break;
+    case 12:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Queen_of_spades_en.svg/1200px-Queen_of_spades_en.svg.png";
+      break;
+    case 13:
+      cardNumber =
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/King_of_spades_en.svg/1200px-King_of_spades_en.svg.png";
+      break;
+  }
   if (randNumber > 10) {
+    cardsGallery.innerHTML += `<img src=${cardNumber} />`;
     return 10;
   } else if (randNumber === 1) {
+    cardsGallery.innerHTML += `<img src=${cardNumber} />`;
     return 11;
   } else {
+    cardsGallery.innerHTML += `<img src=${cardNumber} />`;
     return randNumber;
   }
 }
 
 function playGame() {
   cardEl.textContent = "Carte: ";
+
   for (let i = 0; i < cardsArr.length; i++) {
     cardEl.textContent += cardsArr[i] + " ";
   }
@@ -38,7 +99,7 @@ function playGame() {
     gameWon++;
     gamesWon.textContent = "Partite vinte: " + gameWon;
   } else {
-    response = "Spiacente, sei fuori!";
+    response = "Il banco vince!";
     isAlive = false;
   }
 
@@ -47,6 +108,7 @@ function playGame() {
 
 function startGame() {
   if (!isAlive || hasBlackjack) {
+    cardsGallery.innerHTML = "";
     isAlive = true;
     hasBlackjack = false;
     gamePlayed++;
@@ -67,5 +129,3 @@ function newCard() {
     playGame();
   }
 }
-
-console.log("partite giocate", gamePlayed);
